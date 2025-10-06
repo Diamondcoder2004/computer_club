7f0bfc4a55947391fda9ab6c0c6c6ff1fc8f8327
Diamondcoder2004 <almaz_sabitov04@mail.ru>
2025-10-06 22:07:41 +0500

    Ваше сообщение

--- src/services/apiClient.js
+++ src/services/apiClient.js
@@ -5,7 +5,7 @@ import axios from 'axios';
 const apiClient = axios.create({
     //baseURL: 'http://192.168.0.112:3001',
     //baseURL: 'https://curseproject-6.onrender.com',
-    baseURL: 'https://mobile-app-server-1.onrender.com',
+    baseURL: 'https://mobile-app-server-3.onrender.com',
     timeout: 10000,
     headers: {
         'Content-Type': 'application/json',
